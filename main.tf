6bcb98df9a93c48762b21683a7eaa5114f1a9040
lgswin <glee6973@conestogac.on.ca>
2024-12-06 23:15:22 -0500

    add terraform configuration #1

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-    ami = "ami-0c55b159cbfafe1f0"
+    ami = "ami-0c55b159cbfafelf0"
     instance_type = "t2.micro"
     count = var.core_count
 
